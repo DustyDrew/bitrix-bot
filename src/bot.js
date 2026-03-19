5aa8bf8efc3274f32df08c1dc84cf7da9cd383ff
DyDw <dustydrewone@gmail.com>
2026-03-19 17:05:49 +0300

    Move files to src/ to match Render's expectation

---
+++ src/bot.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+app.use(express.json({ limit: '10mb' }));
+app.post('/bot.js', (req, res) => {
+     const event = req.body.event;
+     const data = req.body.data;
+     console.log('Получено событие:', event);
+     if (event === 'message.new') {
+     const message = data.message;
+     console.log('Новое сообщение:', message.text);
+     fetch('https://fdm76.bitrix24.ru/rest/4489/evcj82soys65ao2y/message.send', {
+         method: 'POST',
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify({
+         chat_id: data.chat_id,
+         message: 'Бот получил сообщение: ' + message.text
+         })
+     });
+     }
+     res.status(200).send('OK');
+});
+app.listen(3000, () => {
+     console.log('Бот запущен на http://localhost:3000/bot.js');
+});
